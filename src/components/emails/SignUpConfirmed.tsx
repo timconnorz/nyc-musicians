6eea5d6f425e31ef0109e30183fecfe5d21465d8
Tim Connors <tim@rewind.ai>
2024-07-31 14:55:47 -0400

    fixed email design for sign up confirmation

--- src/components/emails/SignUpConfirmed.tsx
+++ src/components/emails/SignUpConfirmed.tsx
@@ -3,14 +3,50 @@ import * as React from 'react';
 interface SignUpConfirmedProps {}
 
 const SignUpConfirmed: React.FC<Readonly<SignUpConfirmedProps>> = () => (
-  <div>
-    <h3>Thanks for signing up!</h3>
-    <p>
+  <div
+    style={{
+      fontFamily: "'Circular', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+      color: '#181818',
+      backgroundColor: '#ffffff',
+      padding: '20px',
+      maxWidth: '600px',
+      margin: '0 auto',
+    }}
+  >
+    <h1
+      style={{
+        fontSize: '32px',
+        fontWeight: 'bold',
+        color: '#1DB954',
+        marginBottom: '20px',
+      }}
+    >
+      Thanks for signing up!
+    </h1>
+    <p style={{ fontSize: '16px', lineHeight: '1.5', marginBottom: '20px' }}>
       You'll now receive our newsletter with the latest music opportunities in
       NYC.
     </p>
-    <p>--</p>
-    <a href='https://nycmusicianswanted.com'>NYC Musicians Wanted</a>
+    <div
+      style={{
+        borderTop: '1px solid #DDDDDD',
+        paddingTop: '20px',
+        marginTop: '20px',
+        textAlign: 'center',
+      }}
+    >
+      <a
+        href='https://nycmusicianswanted.com'
+        style={{
+          color: '#1DB954',
+          textDecoration: 'none',
+          fontSize: '14px',
+          fontWeight: 'bold',
+        }}
+      >
+        NYC Musicians Wanted
+      </a>
+    </div>
   </div>
 );
 
